54a88669ac6db9a0740d4f5402659d8221d6a6cc
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2022-11-21 17:17:36 -0500

    Primera fase(login-completed)

---
+++ Config/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+const base_url = "http://localhost/CrudPHP/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "adminltemvc";
+ const charset = "charset=utf8";
+
+?>
---
+++ Views/Templates/footer.php
@@ -0,0 +1,27 @@
+
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2022</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        
+
+        <script src="<?php echo base_url;?>Assets/js/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url;?>Assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url;?>Assets/js/scripts.js"></script>
+        <script src="<?php echo base_url;?>Assets/js/Chart.bundle.min.js"  crossorigin="anonymous"></script>
+        <script src="<?php echo base_url;?>Assets/demo/chart-area-demo.js"></script>
+        <script src="<?php echo base_url;?>Assets/demo/chart-bar-demo.js"></script>
+        <script src="<?php echo base_url;?>Assets/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url;?>Assets/demo/datatables-demo.js"></script>
+    </body>
+</html>
