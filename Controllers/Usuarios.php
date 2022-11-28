6223b605ab684e059b5a72326ba18161f6842bd1
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2022-11-28 09:27:26 -0500

    login-usuarios(completado)

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -3,6 +3,9 @@
 class Usuarios extends Controller{
     public function __construct(){
     session_start();
+    if (empty($_SESSION['activo'])) {
+      header("location: " .base_url);
+   }
 
     parent::__construct();
     }
